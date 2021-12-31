536bd097d8043d47f64ac7d102964ec431fb78fc
Maren Arnhold <info@kotori.de>
2021-12-31 07:56:18 +0100

    Add files via upload

--- demo.php
+++ demo.php
@@ -1,10 +1,9 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
 ini_set("max_execution_time", -1);
 
-require("./lib/weihnachtsmilljoeh.php");
+require("./src/weihnachtsmilljoeh.php");
+
 use Kotori\Weihnachtsmilljoeh as WM;
 
 $wm = new WM(9.223372036854770*pow(10,18),9.223372036854775*pow(10,18));
